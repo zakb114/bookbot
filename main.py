c0610c09f38e30b760a4c95e446322b2dbc18374
zakb114 <zakbantell@gmail.com>
2024-10-13 12:04:53 -0700

    added sorting function

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words_number = how_many_words(text)
-    letters = num_letters(text)
+    letters = sort_on(text)
+    
     print(f"The book contains {words_number} words.")
     print("\nLetter counts:")
-    for char, count in letters.items():
+    for char, count in letters:
         if char.isalpha():
             print(f"The '{char}' character was found {count} times")
 
@@ -25,7 +26,10 @@ def num_letters(text):
         num_dict[char] = num_dict.get(char, 0) + 1
     return num_dict 
 
-def sort_on(dict):
-    return dict["num"]
-
+def sort_on(text):
+    letter_counts = num_letters(text)
+    sort_letters = list(letter_counts.items())
+    sort_letters.sort(key=lambda x: x[1], reverse=True)
+    return sort_letters
+    
 main()
